908b53ef8eb80353fd69bfd657f2fe5622f90281
Teddynight <tanyuliang2@gmail.com>
2023-08-20 19:05:39 +0800

    util: sleep

---
+++ user/sleep.c
@@ -0,0 +1,19 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+    int time;
+    if (argc <= 1 || argc >= 3) {
+        fprintf(2, "sleep: require an argument");
+        exit(1);
+    }
+    time = atoi(argv[1]); 
+    if (time < 0) {
+        fprintf(2, "sleep: invalid argument");
+        exit(1);
+    }
+    sleep(time);
+    exit(0);
+}
