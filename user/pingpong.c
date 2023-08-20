514bf8aaf2d746eda3b761fda13ca2f7c1a1dcc9
Teddynight <tanyuliang2@gmail.com>
2023-08-20 21:46:12 +0800

    util: pingpong

---
+++ user/pingpong.c
@@ -0,0 +1,29 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+    int p1[2];
+    int p2[2];
+    int pid;
+    pipe(p1);
+    pipe(p2);
+    pid = fork();
+    if (pid == 0) {
+        pid = getpid();
+        char c;
+        read(p1[1], &c, 1);
+        fprintf(1, "%d: received ping\n", pid);
+        write(p2[0], "\n", 1);
+    } else {
+        pid = getpid();
+        write(p1[0], "\n", 1);
+        wait(&pid);
+        pid = getpid();
+        char c;
+        read(p2[1], &c, 1);
+        fprintf(1, "%d: received pong\n", pid);
+    }
+    exit(0);
+}
